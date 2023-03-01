98cd05371dd3cb942f9cd3d726eacce1a1efb220
e1drah <113189459+e1drah@users.noreply.github.com>
2023-03-01 15:33:38 -0400

    Continued to iron out bugs

--- Assets/BoolController.cs
+++ Assets/BoolController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class BoolController : MonoBehaviour
 {
-    private Animator animator;
-    // Start is called before the first frame update
+    public Animator animator;
+
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if ((Input.GetKeyDown(KeyCode.Space)) && (animator.GetBool("isRotating")))
